98cd64654b9d92d0070f5ae46e5795f4655a92e6
Maleny <maleny.dominguez99@gmail.com>
2025-11-27 17:09:23 -0600

    gestion lista studen con inscripcion y consutas

--- Courses.java
+++ Courses.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Courses {
-
-}
+	String coursesName;
+	String professorName;
+	int year;
+	
+	List<Student> students;
+	
+	public Courses(String coursesName, String professorName, int year) {
+		this.coursesName = coursesName;
+		this.professorName = professorName;
+		this.year = year;
+		this.students = new ArrayList<>();
+	}//constructor 
+	
+	public void enroll(Student student) {
+		students.add(student);
+	}//enroll
+	
+	public void unEnroll(Student student) {
+		if (students.contains(student)) {
+			students.remove(student);
+		}
+	}//enroll
+	
+	public int countStudents() {
+		return students.size();
+	}//countStudents
+	
+	public int bestGrade() {
+		int best =0;
+		for (Student s: students) {
+			if(s.grade > best) {
+				best = s.grade;
+			}
+		}
+		return best;
+	}//bestGrade
+	
+}//couses
--- Student.java
+++ Student.java
@@ -1,4 +1,43 @@
-
 public class Student {
+       String firstName;
+       String lastName;
+       int registration;
+       int grade;
+       int year;
+       
+       public Student() {
+       }
+       
+       public Student(String firstName, String lastName) {
+    	   this.firstName = firstName;
+    	   this.lastName = lastName;
+       }
+       
+       public Student(String firstName, String lastName, int registration, int grade, int year) {
+    	   this.firstName = firstName;
+    	   this.lastName = lastName;
+    	   this.registration = registration;
+    	   this.grade = grade;
+    	   this.year = year;
+       }
+       
+       public void printFullName(){
+    	   System.out.println(firstName + " " + lastName);
+    	   }//fullName
 
-}
+    	   public boolean isApproved() {
+    	       //TODO implement: should return true if grade >= 60
+    		   return grade >= 60;
+    		}//approved
+
+    	   public int changeYearIfApproved(){
+    	       //TODO implement: the student should advance to the next year if he/she grade is >= 60
+    	       // Make year = year + 1, and print "Congragulations" if the student has been approved
+    	       if (isApproved()) {
+    	    	   year = year + 1;
+    	    	   System.out.println("Congratulations");
+    	       }//if isApproved
+    	       return year;
+    	   }//changeYearIfApproved
+       
+}//student 
